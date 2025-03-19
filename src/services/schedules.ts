7fd5b94cbb8c7ddd5c1338be4623c90cc4c59bcd
jrjhjeje12 <jrhee2021@gmail.com>
2025-03-19 10:03:35 -0400

    set mock data

--- src/services/schedules.ts
+++ src/services/schedules.ts
@@ -1,41 +1,41 @@
 /**
  * Mock data
  */
-// import { mockScheduleDays } from "@/mocks/mockData";
-// import type { ScheduleDay } from "@/types/schedule";
-
-// export async function fetchSchedules(): Promise<ScheduleDay[]> {
-//     return mockScheduleDays;
-//     // const json = await new Promise<string>((resolve) => {
-//     //     window.OMNative.getSchedules((json) => {
-//     //         resolve(json);
-//     //     });
-//     // })
-
-//     // const schedules = JSON.parse(json);
-//     // return schedules;
-// }
-
-// export async function openSchedule(scheduleId: string): Promise<void> {
-//     return await window.OMNative.openSchedule(scheduleId);
-// }
-
-/**
- * OM Native
- */
+import { mockScheduleDays } from "@/mocks/mockData";
 import type { ScheduleDay } from "@/types/schedule";
 
 export async function fetchSchedules(): Promise<ScheduleDay[]> {
-    const json = await new Promise<string>((resolve) => {
-        window.OMNative.getSchedules((json) => {
-            resolve(json);
-        });
-    });
+    return mockScheduleDays;
+    // const json = await new Promise<string>((resolve) => {
+    //     window.OMNative.getSchedules((json) => {
+    //         resolve(json);
+    //     });
+    // })
 
-    const schedules = JSON.parse(json);
-    return schedules;
+    // const schedules = JSON.parse(json);
+    // return schedules;
 }
 
 export async function openSchedule(scheduleId: string): Promise<void> {
     return await window.OMNative.openSchedule(scheduleId);
 }
+
+/**
+ * OM Native
+ */
+// import type { ScheduleDay } from "@/types/schedule";
+
+// export async function fetchSchedules(): Promise<ScheduleDay[]> {
+//     const json = await new Promise<string>((resolve) => {
+//         window.OMNative.getSchedules((json) => {
+//             resolve(json);
+//         });
+//     });
+
+//     const schedules = JSON.parse(json);
+//     return schedules;
+// }
+
+// export async function openSchedule(scheduleId: string): Promise<void> {
+//     return await window.OMNative.openSchedule(scheduleId);
+// }
